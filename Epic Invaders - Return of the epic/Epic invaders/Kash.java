7626219f3a7bd5f30ceeca6bd9577776722cf0be
Aadhi <115016177+aadh1@users.noreply.github.com>
2023-01-25 18:13:08 +0000

    Add files via upload

---
+++ Epic Invaders - Return of the epic/Epic invaders/Kash.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Kash here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kash extends Zorg
+{
+    /**
+     * Act - do whatever the Kash wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Kash()
+    {
+        setRotation(90);
+    }
+    public void act()
+    {
+       moveZorg(); 
+    }
+            
+}
