4201c6e18d06d2e7672bbe404833b28dedfa1c4a
Aadhi <aadhi_np@hotmail.com>
2023-01-25 16:27:58 +0000

    Initial commit

---
+++ Epic Invaders - Return of the epic-copy/UFO.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class UFO here.
+ * 
+ * @author (Aadhi, Crown, Harris) 
+ * @version (20/01/23)
+ */
+public class UFO extends Actor
+{
+    /**
+     * Act - do whatever the UFO wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
