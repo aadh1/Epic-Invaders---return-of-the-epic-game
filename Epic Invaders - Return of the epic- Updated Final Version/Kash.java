fe5fcdb6a57040117ee47675dfc5c029e1e5ac86
Aadhi <115016177+aadh1@users.noreply.github.com>
2023-01-27 02:16:54 +0000

    Add files via upload
    This is the final version of the game, fully updated prior the previous versions had issues saving world.

---
+++ Epic Invaders - Return of the epic- Updated Final Version/Kash.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Kash here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kash extends Zorg
+{
+    /**
+     * Act - do whatever the Kash wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Kash()
+    {
+        setRotation(90);
+    }
+    public void act()
+    {
+       moveZorg();
+       hitByboom();
+    }
+    public void hitByboom()
+    {
+        Actor boom = getOneIntersectingObject(boom.class);
+        if  (boom != null)
+        {
+            getWorld().removeObject(boom);
+            World world = getWorld();
+            MyWorld myWorld = (MyWorld)world;
+            Counter counter = myWorld.getCounter();
+            counter.addScore();
+            getWorld().removeObject(this);
+        }
+        else if (getY()==599)
+        {
+            World world = getWorld();
+            MyWorld myWorld = (MyWorld)world;
+            Healthbar healthbar = myWorld.gethealthbar();
+            healthbar.loseHealth();
+            getWorld(). removeObject(this);
+        }
+    }
+            
+}
---
+++ Epic Invaders - Return of the epic- Updated Final Version/MyWorld.java
@@ -0,0 +1,65 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (Aadhi) 
+ * @version (20/01/23)
+ */
+public class MyWorld extends World
+{
+    int zorgCount=0;
+    Healthbar healthbar = new Healthbar();
+    Counter counter = new Counter();
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 600, 1); 
+        prepare();
+    }
+    public Counter getCounter()
+    {
+        return counter;
+    }
+    public Healthbar gethealthbar()
+    {
+        return healthbar;
+    }
+    public void act()
+    {
+        addKash();
+        addSnat();
+    }
+    public void addKash()
+    {
+        if (Greenfoot.getRandomNumber(120)<1)
+        {
+            addObject(new Kash(), Greenfoot.getRandomNumber(600), 0);
+        }
+    }
+     public void addSnat()
+    {
+        if (Greenfoot.getRandomNumber(200)<1)
+        {
+            addObject(new Snat(), Greenfoot.getRandomNumber(600), 0);
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        addObject(counter, 100, 50);
+        addObject (healthbar, 250, 50);
+        Player player = new Player();
+        addObject(player,252,510);
+        player.setLocation(270,491);
+    } 
+
+}
